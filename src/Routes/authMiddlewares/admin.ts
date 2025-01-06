f4632205f7e744fc3aa9d2a393c787f0aa225b58
mohak <c.mohak2004@gmail.com>
2025-01-07 00:48:05 +0530

    API updates

--- src/Routes/authMiddlewares/admin.ts
+++ src/Routes/authMiddlewares/admin.ts
@@ -1,2 +1,37 @@
 //auth middleware for admins
 // to check if the loggedin one is admin or not. 
+
+import jwt from "jsonwebtoken";
+
+export const adminAuth = (req: any, res: any, next: any) => {
+
+const authHeader = req.headers.authorization;
+
+ if(!authHeader || !authHeader.startsWith('Bearer ') ){
+   return res.status(403).json({
+    msg : "invalid user please login"
+   })
+ }
+
+ const token = authHeader.split(' ')[1];
+
+ try {
+    const decoded = jwt.verify(token, "secret") as {id : string, role: string}
+    
+  if(decoded.role !== "admin"){
+    res.status(403).json({
+        msg: "invalid access"
+    })
+    return;
+  } else if(decoded.id) {
+   req.userId = decoded.id;
+   req.role = decoded.role;
+   next();
+  }
+
+
+ } catch (error) {
+    console.log("error while checking");
+    console.log(error);
+ }
+}
