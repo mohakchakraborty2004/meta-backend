7108dc3d66cbdddd36e82f399fb4fc4c96b065f0
mohak <c.mohak2004@gmail.com>
2025-01-10 11:50:06 +0530

    API updates

--- src/Routes/space.ts
+++ src/Routes/space.ts
@@ -83,20 +83,152 @@ res.status(200).json({
 }
 })
 
-spaceRouter.delete("/:spaceId",userAuth, async(req: any, res: any)=> {
+spaceRouter.delete("/delete/:spaceId",userAuth, async(req: any, res: any)=> {
 //deleting a space created
+try {
+    const spaceId = req.params.spaceId;
+
+const space = await prisma.space.findUnique({
+where : {
+    id : spaceId
+},
+select : {
+    creatorID : true
+}
+})
+
+if(space?.creatorID != req.userId){
+    return res.status(402).json({
+        msg : "user unauthorized"
+    })
+}
+
+const response = await prisma.space.delete({
+    where : {
+        id : spaceId
+    }
+})
+
+res.status(200).json({
+    msg: "space deleted"
+})
+} catch (error) {
+    console.log(error);
+    res.staus(500).json({
+        msg : "internal server error"
+    })
+}
+
 })
 
 spaceRouter.get("/all", userAuth, async(req: any, res: any)=> {
 // get all the existing spaces that the user created
+try {
+    const userId = req.userId;
+
+    const response = await prisma.space.findMany({
+        where : {
+            creatorID : userId
+        }
+    })
+
+    if(!response){
+        return res.status(400).json({
+            msg :  "spaces not found"
+        })
+    }
+
+    res.status(200).json({
+        msg : "spaces fetched", 
+        spaces : response.map(s => ({
+            spaceId : s.id,
+            name : s.name,
+            thumbnail : s.thumbnail,
+            dimensions : `${s.height} x ${s.width}`
+        }))
+    })
+
+    // this is another way :
+
+//     const response1 = await prisma.user.findUnique({
+//         where : {
+//             id : userId
+//         },
+//         select : {
+//             spaces : true
+//         }
+//     })
+
+//     res.status(200).json({
+//         msg : "spaces fetched", 
+//         spaces : response1?.spaces.map(s => ({
+//             spaceId : s.id,
+//             thumbnail : s.thumbnail,
+//             name : s.name
+//         }))
+//     })
+
+
+} catch (error) {
+    console.log(error);
+    res.status(500).json({
+        msg : "internal server error"
+    })
+}
 })
 
 spaceRouter.get("/:spaceId",userAuth, async(req: any, res: any)=> {
 // get the selected space info i.e all the elements existing
+try {
+    const userId = req.userId;
+    const spaceId =  req.params.spaceId
+
+    const space = await prisma.space.findUnique({
+        where : {
+            id : spaceId
+        }
+    })
+
+    if(space?.creatorID != userId || !space){
+        return res.json({
+            msg : "either user unauthorized or the space doesnt exists"
+        })
+    }
+
+    const spaceElements = await prisma.spaceElements.findMany({
+        where : {
+            spaceID : spaceId
+        }
+    })
+     
+    if(spaceElements){
+      return  res.status(200).json({
+            msg : "elements fetched", 
+            elements : spaceElements.map(s => ({
+                id : s.id,
+                elementId : s.elementID,
+                x : s.height,
+                y : s.width
+            }))
+        })
+    }
+
+    
+} catch (error) {
+    console.log(error); 
+    res.status(500).json({
+        msg : "internal server error"
+    })
+}
 })
 
 spaceRouter.post("/element",userAuth, async(req: any, res: any)=> {
     //add an element into the space 
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
 spaceRouter.delete("/RemoveElement",userAuth, async(req: any, res: any)=> {
