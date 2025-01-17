755a1df57cef7f23cca7f1a4adfab5290b365718
mohak <c.mohak2004@gmail.com>
2025-01-18 00:31:03 +0530

    WS updates

--- src/wsServer/spaceManager.ts
+++ src/wsServer/spaceManager.ts
@@ -1,4 +1,5 @@
 import WebSocket from "ws";
+import prisma from "../db";
 
 enum Action {
     join,
@@ -17,17 +18,65 @@ interface move {
     y: Number
 }
 
+async function FindspaceID( spaceId : string) {
+   const response = await prisma.space.findUnique({
+where : {
+    id : spaceId
+}
+ })
+ if(response){
+    return true;
+ } else {
+    return false;
+ } 
+}
+
+// create a spaceId Map that fetches the space ids and stores it in the map against set of sockets.
+type spaceMap = Map<String, Set<WebSocket>>;
+
 class spaceManager {
 
-    public join(){
+    private spaces: spaceMap;
+
+    constructor() {
+        this.spaces = new Map();
+    }
+
+    public async join (spaceId : string, ws: WebSocket){
+        //check if the spaceId given by user is in the db or not. (done)
+        const response = await FindspaceID(spaceId);
+
+        if(response) {
+
+            if(!this.spaces.has(spaceId)){
+                this.spaces.set(spaceId, new Set());
+                this.spaces.get(spaceId)?.add(ws);
+                console.log("space added to map");
+                //subscribtion addition here
+                ws.send(JSON.stringify("Space Joined"));
+            } else {
+                this.spaces.get(spaceId)?.add(ws);
+            }
+        } else {
+            ws.send(JSON.stringify("No Space found for that ID"))
+        }
+
+       
+        //make him join the server (done)
+        //subscibe on join 
+        // make server send a message that space joined(done)
+
 
     }
 
     public handleMessage(){
+        //check for the jwt authorization 
 
     }
 
     public Move(){
-        
+        //make a move 
+        // (publish) send to all the other websocket joined the move and update on frontend accordingly.
+        //make server send a message that movement is made.
     }
 }
